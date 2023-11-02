777a2a2065d632e69c66ec8def0a3cc392ec66d2
ran81998 <149679702+ran81998@users.noreply.github.com>
2023-11-02 18:06:01 +0300

    Add files via upload

---
+++ b.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".btn");
+        
+
+        btn.addEventListener("click",()=>{
+       
+            document.body.classList.toggle("active")
+        })
+
+       
